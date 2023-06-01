656cf9b5a3f62b24456f89c324b42b51181a4380
geetamatkar <geetamatkar1@gmail.com>
2023-05-31 23:14:38 -0500

    Completed backend

--- index.js
+++ index.js
@@ -4,5 +4,9 @@ const port = process.env.PORT || 5000;
 
 const app = express();
 
+//enable body parser
+app.use(express.json());
+app.use(express.urlencoded({extended: false}))
+
 app.use('/openai', require('./routes/openaiRoutes'));
 app.listen(port, () => console.log(`Server started on port ${port}`));
