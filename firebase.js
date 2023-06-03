a0abcc500394d18165141ef781ab7910bd5e7c9a
geetamatkar <geetamatkar1@gmail.com>
2023-06-03 11:42:43 -0500

    # dev commit

---
+++ firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyC3ZsXPVCLzQAlgiwbs1AkBPba9yFjSo3E",
+    authDomain: "image-generator-9b365.firebaseapp.com",
+    projectId: "image-generator-9b365",
+    storageBucket: "image-generator-9b365.appspot.com",
+    messagingSenderId: "98134554680",
+    appId: "1:98134554680:web:9ca71a78cd67b6c19e8ecd"
+  };
